8d73e382f17dd6394def323b7622cfc96d8bdef8
Tomaz Stih <tomaz.stih@wischner.co.uk>
2025-09-01 19:18:10 +0100

    Removed build

--- build/zxspec48-app/main.adb
+++
@@ -1,7 +0,0 @@
-M:main
-F:G$main$0_0$0({2}DF,SV:S),C,0,0,0,0,0
-S:G$cinit$0_0$0({2}DF,SV:S),C,0,0
-S:G$cputc$0_0$0({2}DF,SV:S),C,0,0
-S:G$cputs$0_0$0({2}DF,SV:S),C,0,0
-S:G$cclear$0_0$0({2}DF,SV:S),C,0,0
-S:G$main$0_0$0({2}DF,SV:S),C,0,0
